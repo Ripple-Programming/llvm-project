d6ead3b3a827915f8377cf22a766a70cd9ea24c8
Maxime Schmitt <maxime.schmitt@qti.qualcomm.com>
2025-08-28 07:28:10 -0400

    Ripple RFC: clang
    This pass contains all the Clang front-end changes to suppor Ripple:
    
    - ripple.h: C/C++ ripple interface supported by clang builtins
    - semantic analysis for Ripple constructs
    - ripple parallel, a semantics transformation, pragma and call API
      support through a RippleComputeConstruct statement.
    
    Co-authored-by: Benoît Meister <benoit.meister@qti.qualcomm.com>
    Co-authored-by: Eli Friedman <efriedma@quicinc.com>
    Co-authored-by: Sergei Larin <slarin@qti.qualcomm.com>
    Co-authored-by: Zhaoshi Zheng <zhaoshi.zheng@quicinc.com>

--- clang/include/clang/Parse/Parser.h
+++ clang/include/clang/Parse/Parser.h
@@ -7051,6 +7051,7 @@ private:
   std::unique_ptr<PragmaHandler> MaxTokensHerePragmaHandler;
   std::unique_ptr<PragmaHandler> MaxTokensTotalPragmaHandler;
   std::unique_ptr<PragmaHandler> RISCVPragmaHandler;
+  std::unique_ptr<PragmaHandler> RippleHandler;
 
   /// Initialize all pragma handlers.
   void initializePragmaHandlers();
--- clang/lib/AST/StmtProfile.cpp
+++ clang/lib/AST/StmtProfile.cpp
@@ -2908,6 +2908,11 @@ void StmtProfiler::VisitHLSLOutArgExpr(const HLSLOutArgExpr *S) {
   VisitStmt(S);
 }
 
+void StmtProfiler::VisitRippleComputeConstruct(
+    const RippleComputeConstruct *S) {
+  VisitStmt(S);
+}
+
 void Stmt::Profile(llvm::FoldingSetNodeID &ID, const ASTContext &Context,
                    bool Canonical, bool ProfileLambdaExpr) const {
   StmtProfilerWithPointers Profiler(ID, Context, Canonical, ProfileLambdaExpr);
