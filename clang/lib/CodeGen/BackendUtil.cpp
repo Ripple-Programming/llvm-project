9233c442d16dd9f0c2dc28cd8f50ebe90452061b
Maxime Schmitt <maxime.schmitt@qti.qualcomm.com>
2025-09-23 09:30:53 -0400

    [Ripple][Clang] Driver fix for IR/precompiled inputs
    The driver was not parsing the `-fenable-ripple` flag when compiling
    pre-compiler or IR inputs, breaking the `-save-temps` option.
    
    Marshalling a CodeGenOption and parsing `-fenable-ripple` as a compiler
    option solves the issue of registering the ripple pass when compiling IR
    and precompiled headers.
    
    Reviewed-by: Sergei Larin <slarin@qti.qualcomm.com>

--- clang/lib/CodeGen/BackendUtil.cpp
+++ clang/lib/CodeGen/BackendUtil.cpp
@@ -1164,7 +1164,10 @@ void EmitAssemblyHelper::RunOptimizationPipeline(
       });
     }
 
-    if (LangOpts.Ripple)
+    //
+    // Ripple
+    ///
+    if (CodeGenOpts.EnableRipple)
       PB.registerOptimizerEarlyEPCallback(
           [TM = std::ref(TM)](ModulePassManager &MPM, OptimizationLevel Level,
                               ThinOrFullLTOPhase Phase) {
